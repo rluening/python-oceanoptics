7d2b7793093d81a5074f634736965296d3ca0af0
rluening <ralf.luening@hs-bremen.de>
2014-10-15 12:30:22 +0200

    Update XXX2000.py
    Tested with an USB2000

--- oceanoptics/spectrometers/XXX2000.py
+++ oceanoptics/spectrometers/XXX2000.py
@@ -9,39 +9,47 @@ import struct
 
 class _XXX2000(_OOBase):
 
+   def _build16bit(self,spec_raw):
+        """ Despite saying 16 bit, max is 12bit due to ADC """
+        r64=range(64)    
+        spec=[0]*64*32
+        k=0
+        for i in range(32):
+            for ii in r64:
+                #t[i][ii]= MSB | LSB
+                spec[k]=( ((spec_raw[2*i+1][ii])<<8) | spec_raw[2*i][ii] )
+                k+=1
+        return spec
+        
+    #Get a spectrum
     def _request_spectrum(self):
-        self._usb_send(struct.pack('<B', 0x09))
-        time.sleep(max(self._integration_time - self._USBTIMEOUT, 0))
-        ret = [ self._usb_read(epi=self._EPspec, epi_size=self._packet_size)
-                            for _ in range(self._packet_N) ]
-        ret = sum( ret[1:], ret[0] )
-
-        # XXX: This sorts the the packets in the right order
-        ret = "".join(ret[j*self._packet_size + i] + ret[(j+1)*self._packet_size + i]
-                      for j in range(self._packet_N) for i in range(self._packet_size))
-
-        sync = self._usb_read(epi=self._EPspec, epi_size=1)
+        self._usb_send(struct.pack('<B',0x09))
+        time.sleep(self._integration_time/1000.00)
+       # time.sleep(max(self._integration_time - self._USBTIMEOUT, 0))
+        spec_raw = [[0]*64]*64
+        for i in range(64):
+           spec_raw[i] = self._usb_read( epi=0x82, epi_size=64)
+        sync= self._usb_read( epi=0x82, epi_size=1)
         if sync[0] != 0x69:
             raise _OOError('request_spectrum: Wrong sync byte')
-        spectrum = struct.unpack('<'+'H'*self._pixels, ret)
-        spectrum = map(self._packet_func, spectrum)
+        #now sorting the data
+        spectrum= self._build16bit(spec_raw)
         return spectrum
+        
+    def _query_information(self,index):
+             self._usb_send(struct.pack('<BB',0x05,int(index)))
+             tt=self._usb_read(epi=0x87, epi_size=17)
+             info=tt[2:tt[2:].index(0)+2].tostring()
+             return info
 
-
-
-
-class USB2000(_XXX2000):
-
-    def __init__(self):
-        super(USB2000, self).__init__('USB2000')
 
     def _set_integration_time(self, time_us):
         """ send command 0x02 """
         # XXX: The USB2000 requires the time set in Milliseconds!
         #      This overides the provided function of OOBase
         time_ms = int(time_us/1000)
-        self._usb_send(struct.pack('<BI', 0x02, time_ms))
-
+        self._usb_send(struct.pack('<BH', 0x02,time_ms))
+        
     def _query_status(self):
         """ 0xFE query status """
         # XXX: The USB2000 also returns the time in Milliseconds!
@@ -51,18 +59,28 @@ class USB2000(_XXX2000):
         #      but we return 0x00 to use the abstraction in OOBase
 
         ret = self._usb_query(struct.pack('<B', 0xFE))
-        data = struct.unpack('<HLBBBBBBBBBB', ret[:])
-        ret = { 'pixels' : data[0],
-                'integration_time' : data[1] * 1000,  # ms to us
+        data = struct.unpack('<HHBBBBBBBBBBH', ret[:])
+        pix, inte =struct.unpack('>H H', ret[0:4])
+        ret2 = { 'pixels' : pix,
+                'integration_time' :inte,
                 'lamp_enable' : data[2],
                 'trigger_mode' : data[3],
                 'acquisition_status' : data[4],
-                'packets_in_spectrum' : data[5],
-                'power_down' : data[6],
-                'packets_in_endpoint' : data[7],
-                'usb_speed' : 0x00 }
-        return ret
+                'Timer Swap' : data[5],
+                'Spectral Data Ready' : data[6],
+                'packets_in_endpoint' : 64,
+                'usb_speed' : data[10] }
+        return ret2    
 
+
+
+
+class USB2000(_XXX2000):
+
+    def __init__(self):
+        super(USB2000, self).__init__('USB2000')
+
+    
 
 
 class HR2000(_XXX2000):
@@ -70,31 +88,4 @@ class HR2000(_XXX2000):
     def __init__(self):
         super(HR2000, self).__init__('HR2000')
 
-    def _set_integration_time(self, time_us):
-        """ send command 0x02 """
-        # XXX: The HR2000 requires the time set in Milliseconds!
-        #      This overides the provided function of OOBase
-        # XXX: We also need to save the integration time internally
-        #      because it does not support the _qeury_status command
-        time_ms = int(time_us/1000)
-        self._integration_time = time_ms * 1e3
-        self._usb_send(struct.pack('<BI', 0x02, time_ms))
-
-    def _query_status(self):
-        """ 0xFE query status """
-        # XXX: The HR2000 also returns the time in Milliseconds!
-        #      This overides the provided function of OOBase
-        #      and pretends to return us
-        # XXX: query status does not actually return the usb_speed,
-        #      but we return 0x00 to use the abstraction in OOBase
-
-        ret = { 'pixels' : 2048,
-                'integration_time' : getattr(self, '_integration_time', None),
-                'lamp_enable' : 0,
-                'trigger_mode' : 0,
-                'acquisition_status' : 0,
-                'packets_in_spectrum' : 0,
-                'power_down' : 0,
-                'packets_in_endpoint' : 0,
-                'usb_speed' : 0x00 }
-        return ret
+   
